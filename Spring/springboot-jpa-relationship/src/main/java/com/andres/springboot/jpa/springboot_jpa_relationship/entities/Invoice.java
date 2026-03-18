73ec82e6dd01abf5e95a4c2039fdb85a1bf65e3c
Andres Luna <miosesfreddy123@gmail.com>
2026-03-18 16:19:54 +0100

    La anotación @JoinColumn

--- Spring/springboot-jpa-relationship/src/main/java/com/andres/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
+++ Spring/springboot-jpa-relationship/src/main/java/com/andres/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -20,6 +21,7 @@ public class Invoice {
 
     //relaciones
     @ManyToOne
+    @JoinColumn(name="id_cliente_temp")
     private Client client;
 
 
